f4b4420f5c62105afd3afb8b9139be43ba9aa8ff
Paul HALLEUX <account@paulhalleux.be>
2024-04-26 20:50:56 +0200

    chore: tailwind setup

---
+++ configs/toolbox-tailwind-config/tailwind-preset.ts
@@ -0,0 +1,10 @@
+import { PresetsConfig } from "tailwindcss/types/config";
+
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./storybook/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {},
+} as PresetsConfig;
